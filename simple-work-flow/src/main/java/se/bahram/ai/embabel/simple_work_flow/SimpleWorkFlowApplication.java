9d81931a8f850fc5fb0f4de668a9de05ba6a71ed
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-09-20 19:09:56 +0200

    Add embabel to pom.xml

---
+++ simple-work-flow/src/main/java/se/bahram/ai/embabel/simple_work_flow/SimpleWorkFlowApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.embabel.simple_work_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWorkFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWorkFlowApplication.class, args);
+	}
+
+}
---
+++ simple-work-flow/src/test/java/se/bahram/ai/embabel/simple_work_flow/SimpleWorkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.embabel.simple_work_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWorkFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
