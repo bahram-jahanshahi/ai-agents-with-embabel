6655e13e4ec8d6b3ec690c61bc3a10d5e0c2281c
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-10-11 13:48:17 +0200

    Init GitHub Official MCP Client

---
+++ github-official-mcp-client-with-java/src/main/java/se/bahram/ai/mcp/github_official_mcp_client_with_java/GithubOfficialMcpClientWithJavaApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.mcp.github_official_mcp_client_with_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubOfficialMcpClientWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubOfficialMcpClientWithJavaApplication.class, args);
+	}
+
+}
---
+++ github-official-mcp-client-with-java/src/test/java/se/bahram/ai/mcp/github_official_mcp_client_with_java/GithubOfficialMcpClientWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.mcp.github_official_mcp_client_with_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubOfficialMcpClientWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
